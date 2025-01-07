ac081a036fb8fa9be31b46963df2bad1c0c3e3a0
Daniel Paz <dpazrincon@gmail.com>
2025-01-07 05:24:38 +0100

    Portfolio v1.0
    portfolio basic info

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template >
+    <footer class="mt-8 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+        <div class="container p-12 flex justify-between">
+            <div class="text-white text-3xl font-bold">
+                <img src="/src/assets/logo.png" alt="LOGO" width="50" height="50">
+            </div>
+            <p class="text-slate-600">dpazrincon@gmail.com</p>
+        </div>
+    </footer>
+</template>
